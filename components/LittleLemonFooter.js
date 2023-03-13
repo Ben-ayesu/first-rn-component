2f540dbbf28d2f51182c027414d08da9f412dd48
benjaminayesu-attah <ben.ayesu.attah@gmail.com>
2023-03-12 20:55:40 -0700

    LittleLemonFooter stylesheet completed

--- components/LittleLemonFooter.js
+++ components/LittleLemonFooter.js
@@ -1,12 +1,25 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 export default function Footer() {
   return (
-    <View style={{ backgroundColor: '#F4CE14', marginBottom: 10 }}>
-      <Text style={{ fontSize: 18, color: 'black', textAlign: 'center' }}>
-        All rights reserved by Little Lemon, 2022{' '}
+    <View style={LittleLemonFooterStyles.footerContainer}>
+      <Text style={LittleLemonFooterStyles.footerText}>
+        All rights reserved by Little Lemon, 2022
       </Text>
     </View>
   );
 }
+
+const LittleLemonFooterStyles = StyleSheet.create({
+  footerContainer: {
+    backgroundColor: '#F4CE14',
+    marginBottom: 10
+  },
+  footerText: {
+    fontSize: 18,
+    color: 'black',
+    textAlign: 'center',
+  }
+})
